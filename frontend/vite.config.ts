7fff9826faaeed3e44bbe9c7a76fc3192bf09683
Vatsal varma <gamesvatsal99@gmail.com>
2026-06-23 13:35:36 +0530

    Configure GitHub Pages deployment for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Text-to-Voice/',
   plugins: [react(), basicSsl()],
   server: {
     proxy: {
